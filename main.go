0caa0802e6c049c67936b5a9c1018949f0c980d8
stm33 <satuma33@icloud.com>
2022-10-28 09:55:56 +0900

    [add]refs #19-slide

--- main.go
+++ main.go
@@ -1,56 +1,14 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getalbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func getAlbumById(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func postalbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/albums", getalbums)
-	r.POST("/albums", postalbums)
-	r.GET("/albums/:id", getAlbumById)
+	var emptiy interface{}
 
-	r.Run(":8080")
+	emptiy = "Hello World"
+	emptiy = 100
+	emptiy = true
+
+	fmt.Println(emptiy)
+
 }
