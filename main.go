e242ef9c771fe8c9bab5995fcfd303dc85debb17
stm33 <satuma33@icloud.com>
2022-11-09 20:25:19 +0900

    [add]refs #29 coding-test

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"go-dev/initializers"
-	"go-dev/models"
-)
-
-func init() {
-	initializers.ConnectDB()
-}
-
 func main() {
-	initializers.DB.AutoMigrate(&models.Product{})
 }
