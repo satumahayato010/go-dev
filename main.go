9393ded17e3448c482c3b0f70eee471b087ec0fb
stm33 <satuma33@icloud.com>
2022-11-14 23:23:19 +0900

    [add]refs #35 coding-test5

--- main.go
+++ main.go
@@ -5,21 +5,20 @@ import "fmt"
 func twoSum(nums []int, target int) []int {
 	lookup := make(map[int]int)
 
-	for i, v := range nums {
-		j, ok := lookup[-v]
-		lookup[v-target] = i
-
-		if ok {
-			return []int{j, i}
+	for idx, num := range nums {
+		if pos, ok := lookup[target-num]; ok {
+			return []int{pos, idx}
 		}
+		lookup[num] = idx
 	}
 	return []int{}
 }
 
 func main() {
-	//nums := []int{2, 7, 11, 15}
-	nums2 := []int{3, 2, 4}
-	target := 6
-	output := twoSum(nums2, target)
+	nums := []int{2, 7, 11, 15}
+	var nums2 = []int{2, 7, 11, 15}
+	fmt.Println(nums2)
+	target := 9
+	output := twoSum(nums, target)
 	fmt.Println(output)
 }
