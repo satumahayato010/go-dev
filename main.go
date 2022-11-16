093098cc36d9628ee0a13842a07e144c00f595c8
stm33 <satuma33@icloud.com>
2022-11-16 21:15:59 +0900

    [add]refs #37 coding-test8

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import "fmt"
+
+func totalNumericArray(nums []int) int {
+	var sum int
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
+func arrayMaximumValue(nums []int) int {
+	max := nums[0]
+	for i, _ := range nums {
+		if max < nums[i] {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
 func main() {
+	nums := []int{7, 3, 5, 1, 8, 4}
+	output := totalNumericArray(nums)
+	fmt.Println(output)
+	result := arrayMaximumValue(nums)
+	fmt.Println(result)
 }
