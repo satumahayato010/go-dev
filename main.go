e4bac1dbb4a4f7aec4ad917578362bf83b539f23
stm33 <satuma33@icloud.com>
2022-11-18 15:48:16 +0900

    [add]refs #45 o notation

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-func euclideanAlgorithm(m, n int) int {
-	if n == 0 {
-		return m
-	} else {
-		return euclideanAlgorithm(n, m%n)
+func inOrder(nums []int) bool {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] > nums[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+func shuffle(nums []int) {
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+}
+
+func bogoSort(nums []int) {
+	for !inOrder(nums) {
+		shuffle(nums)
 	}
 }
 
 func main() {
-	output := euclideanAlgorithm(128, 72)
-	fmt.Println(output)
+	nums := []int{1, 5, 3, 2, 6}
+	bogoSort(nums)
+	fmt.Println(nums)
 }
