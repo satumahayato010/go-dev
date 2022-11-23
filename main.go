dde1ed4ac3f8f4c5cb0078e9863f80fdb00a66b2
stm33 <satuma33@icloud.com>
2022-11-23 11:19:54 +0900

    [add]refs #51-coding-test

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
-type Node struct {
-	data int
-	next *Node
+import "fmt"
+
+type Set struct {
+	items map[int]struct{}
 }
 
-type LinkedList struct {
-	head int
+func NewSet() *Set {
+	return &Set{items: make(map[int]struct{})}
 }
 
-func (li *LinkedList) append(data int) {
-	if li.head == 0 {
+func (s *Set) Add(val int) {
+	s.items[val] = struct{}{}
+}
 
+func (s *Set) Has(val int) bool {
+	_, ok := s.items[val]
+	return ok
+}
+
+func containsDuplicate(nums []int) bool {
+	set := NewSet()
+
+	for _, num := range nums {
+		if set.Has(num) {
+			return true
+		}
+		set.Add(num)
+		fmt.Println(set)
 	}
+	return false
 }
 
 func main() {
+	nums := []int{1, 2, 3, 4, 1}
+	output := containsDuplicate(nums)
+	fmt.Println(output)
 }
