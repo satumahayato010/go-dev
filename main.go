24cce31354c75b34435eb506dbcaf75e0612b23f
stm33 <satuma33@icloud.com>
2022-11-26 18:50:36 +0900

    [add]refs #65-first-bad-version

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	nums1 := []int{-1, 0, 3, 5, 9, 12}
-	target := 9
-	low, high := 0, len(nums1)-1
-	output := algorithm.BinarySearchRecursive(nums1, target, low, high)
+	nums := []int{1, 3, 5, 6}
+	target := 7
+	output := algorithm.SearchInsert(nums, target)
 	fmt.Println(output)
+
 }
