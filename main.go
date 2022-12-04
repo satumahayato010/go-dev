701f0ad08283c1e79e8fd45d3ae2bcc66eefd5e5
stm33 <satuma33@icloud.com>
2022-12-04 23:54:30 +0900

    [add]refs #88-add

--- main.go
+++ main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"go-dev/algorithm"
-)
-
-const personName = "tanaka"
-
-func getPersonName() {
-	fmt.Println(personName)
-
-	if true {
-		fmt.Println(personName)
-	}
-}
 func main() {
-	nums := []int{-4, -1, 0, 3, 10}
-	output := algorithm.SortedSquares(nums)
-	fmt.Println(output)
-
-	fmt.Println(personName)
-	getPersonName()
-
 }
